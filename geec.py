dacc20fe884dae1783334b578c6d6a44907f05f5
Paul Demchuk <pademchuk@gmail.com>
2013-09-26 15:08:16 -0700

    added route to get job output

--- geec.py
+++ geec.py
@@ -13,9 +13,10 @@ import json
 class MalformedRequestException(Exception): pass
 
 
-urls = {'newjob': '/job/new',
-        'job': '/job/',
-        'queue': '/queue/'}
+urls = {'newjob'    : '/job/new',
+        'job'       : '/job/',
+        'output'    : '/job/output/',
+        'queue'     : '/queue/'}
 
 
 
@@ -31,6 +32,9 @@ def create_http_url(host, *args):
 def post_request(host, data, headers):
     return requests.post(host, data=data, headers=headers)
 
+def get_request(host, data, headers):
+    return requests.get(host, data=data, headers=headers)
+
 
 def new_job(username, key, jobname, jobfile):
     url = create_http_url(MASTER_SERVER, urls['newjob'])
@@ -38,6 +42,14 @@ def new_job(username, key, jobname, jobfile):
         data = {'user':username, 'key':key, 'data':myfile.read(), 'name':jobname}
     headers = {'content-type':'application/json'}
     return post_request(url, data=json.dumps(data), headers=headers)
+
+def job_output(username, key, jobname):
+    url = create_http_url(MASTER_SERVER, urls['output'], jobname)
+    data = {'user':username, 'key':key, 'name':jobname}
+    headers = {'content-type':'application/json'}
+    r = get_request(url, data=json.dumps(data), headers=headers)
+
+    print r.text
 
 def job_status(username, key, jobname):
     url = create_http_url(MASTER_SERVER, urls['job'], jobname)
@@ -57,7 +69,7 @@ if __name__ == "__main__":
     try:
         user = sys.argv[1]
         key = sys.argv[2]
-        cmdlist = {'queuestat':queue_status, 'jobstat':job_status, 'free':queue_status, 'new':new_job}
+        cmdlist = {'queuestat':queue_status, 'jobstat':job_status, 'free':queue_status, 'new':new_job, 'output': job_output}
         cmd = cmdlist[sys.argv[3]]
         args = sys.argv[4:]
         cmd(user, key, *args)
--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,20 @@
 echo "========== COFFEESCRIPT =========="
-python geec.py "pdem" 1 new "count.coffee" count.coffee
+python ../geec.py "pdem" 1 new "count.coffee" count.coffee
+sleep 0.5
+python ../geec.py "pdem" 1 output "count.coffee"
 echo ""
 echo ""
 
 echo "========== PYTHON =========="
-python geec.py "pdem" 1 new "count.py" count.py
+python ../geec.py "pdem" 1 new "count.py" count.py
+sleep 0.5
+python ../geec.py "pdem" 1 output "count.py"
 echo ""
 echo ""
 
 echo "========== JAVASCRIPT =========="
-python geec.py "pdem" 1 new "count.js" count.js
+python ../geec.py "pdem" 1 new "count.js" count.js
+sleep 0.5
+python ../geec.py "pdem" 1 output "count.js"
+echo ""
+echo ""
